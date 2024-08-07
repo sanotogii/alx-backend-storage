d9fbfb921dc0c05b78913aee84c9b75e0dc9842d
sanotogii <nasaereh@gmail.com>
2024-08-07 21:45:21 +0200

    task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- SQL script that creates a table users
+
+CREATE IF TABLE NOT EXISTS users(
+  id INT NOT NULL AUTO_INCRIMENT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255),
+  country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
