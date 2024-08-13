55279db7bbcd7a374a43f23cf90b72973509b8bc
sanotogii <nasaereh@gmail.com>
2024-08-14 00:33:26 +0200

    task 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,7 +3,7 @@
 from pymongo import MongoClient
 
 
-def log_stats():
+if __name__ == "__main__":
     """Provides stats about Nginx logs stored in MongoDB"""
 
     client = MongoClient('mongodb://127.0.0.1:27017')
