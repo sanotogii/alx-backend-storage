09eeaf84b94a3839adb7d620dee2b2829f98b25c
sanotogii <nasaereh@gmail.com>
2024-08-14 00:20:16 +0200

    task 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Inserts a new document in a collection based on kwargs.
+"""
+from pymongo import MongoClient
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document in a collection based on kwargs.
+    """
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
