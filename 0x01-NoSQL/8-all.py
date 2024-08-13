11de00d61ad0c1712f8b9a53a7a3829ed0cdadff
sanotogii <nasaereh@gmail.com>
2024-08-14 00:12:22 +0200

    task 8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from pymongo import MongoClient
+
+
+def list_all(mongo_collection):
+    """
+    Lists all documents in a collection.
+    """
+    return list(mongo_collection.find())
