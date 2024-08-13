42cdc9f96147e516dc9bacdbe2a6d41b2e8b39f1
sanotogii <nasaereh@gmail.com>
2024-08-14 00:15:04 +0200

    task 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -6,4 +6,6 @@ def list_all(mongo_collection):
     """
     Lists all documents in a collection.
     """
+    if mongo_collection.find().count() == 0:
+        return []
     return list(mongo_collection.find())
