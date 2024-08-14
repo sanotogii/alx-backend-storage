dffaf1db38f8f26879abe99585e820e96d7066d9
sanotogii <nasaereh@gmail.com>
2024-08-14 22:05:17 +0200

    task 4

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -36,6 +36,23 @@ def count_calls(method: Callable) -> Callable:
     
     return wrapper
 
+def replay(method: Callable):
+    """
+    Display the history of calls of a particular function.
+    """
+    cache = method.__self__
+    input_key = f"{method.__qualname__}:inputs"
+    output_key = f"{method.__qualname__}:outputs"
+
+    inputs = cache._redis.lrange(input_key, 0, -1)
+    outputs = cache._redis.lrange(output_key, 0, -1)
+
+    call_count = len(inputs)
+    print(f"{method.__qualname__} was called {call_count} times:")
+    
+    for input_data, output_data in zip(inputs, outputs):
+        print(f"{method.__qualname__}(*{input_data.decode('utf-8')}) -> {output_data.decode('utf-8')}")
+
 class Cache:
     def __init__(self):
         """
